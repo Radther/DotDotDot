46cb1eaa57d2552dd8e10ceb939bc86c54b99c50
Tom Singleton <tomquil13@gmail.com>
2017-01-16 19:56:34 +0000

    Initial Commit

---
+++ DotDotDot/DotDotDot.h
@@ -0,0 +1,19 @@
+//
+//  DotDotDot.h
+//  DotDotDot
+//
+//  Created by Tom Sinlgeton on 16/01/2017.
+//  Copyright © 2017 Tom Sinlgeton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DotDotDot.
+FOUNDATION_EXPORT double DotDotDotVersionNumber;
+
+//! Project version string for DotDotDot.
+FOUNDATION_EXPORT const unsigned char DotDotDotVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DotDotDot/PublicHeader.h>
+
+
